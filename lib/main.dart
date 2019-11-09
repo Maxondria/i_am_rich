cb248be044ea8d90ef08b8f3b4fb794a9f2639d4
Maxon Tayebwa <maxtayebwa@gmail.com>
2019-11-10 01:09:47 +0300

    create a scaffold widget, add an appbar, and an image

--- lib/main.dart
+++ lib/main.dart
@@ -3,8 +3,20 @@ import 'package:flutter/material.dart';
 void main() {
   runApp(
     MaterialApp(
-      home: Center(
-        child: Text('Hello Max...'),
+      home: Scaffold(
+        appBar: AppBar(
+          title: Center(
+            child: const Text('I am Rich'),
+          ),
+          backgroundColor: Colors.blueGrey[900],
+        ),
+        backgroundColor: Colors.blueGrey,
+        body: Center(
+          child: Image(
+            image:
+                NetworkImage('https://www.w3schools.com/w3css/img_lights.jpg'),
+          ),
+        ),
       ),
     ),
   );
